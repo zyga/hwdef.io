338142b9be6c0cf149ab02b3cf8a38eb932a9a94
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2013-04-15 21:39:05 +0200

    hwdefio: hwdef: add and enable the home view
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- hwdefio/hwdef/views.py
+++ hwdefio/hwdef/views.py
@@ -1,1 +1,6 @@
-# Create your views here.
+from django.views.generic import TemplateView
+
+
+class Home(TemplateView):
+
+    template_name = 'index.html'
--- hwdefio/urls.py
+++ hwdefio/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+
+from hwdefio.hwdef.views import Home
 admin.autodiscover()
 
 
 
 urlpatterns = patterns('',
+    url(r'^$', Home.as_view()),
     url(r'^admin/', include(admin.site.urls)),
 )
