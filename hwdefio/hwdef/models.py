5a9210609a278a7af19ba89ed6220be0f2d343e2
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2013-04-15 21:50:23 +0200

    hwdefio: hwdef: set plural name for Hardware
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- hwdefio/hwdef/models.py
+++ hwdefio/hwdef/models.py
@@ -5,3 +5,7 @@ class Hardware(models.Model):
 
     text = models.TextField(
         help_text="This field is free form, fire your creativity!")
+
+    class Meta:
+
+        verbose_name_plural = "hardware"
